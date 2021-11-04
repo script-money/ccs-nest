5084aaad475af9c8be7e82a8984f05e915a7e32c
script-money <74860630+script-money@users.noreply.github.com>
2021-11-04 17:01:48 +0800

    ✨ use fastify

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['debug', 'error', 'verbose', 'warn', 'log'],
-  });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      logger: ['debug', 'error', 'verbose', 'warn', 'log'],
+    },
+  );
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(7001);
+
+  await app.listen(7001, '0.0.0.0');
 }
 bootstrap();
